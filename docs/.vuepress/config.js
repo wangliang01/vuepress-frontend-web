d49b6f34de92f8c38b643a1ad9272e8392fc18d4
王亮 <1392830517@qq.com>
2022-05-10 15:07:14 +0800

    feat: 提交文件内容

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,49 @@
 module.exports = {
-  title: 'Hello VuePress2', // 网站左上角的标题
-  description: 'Just playing around', // <meta name="description" content="Just playing around">
+  title: '前端知识总结', // 网站左上角的标题
+  description: '从各个方面总结前端知识', // <meta name="description" content="Just playing around">
   base: '/vuepress-frontend-web/',
-  configureWebpack: {
-    resolve: {
+  // 主题配置
+  themeConfig: {
+    // 侧边栏
+    sidebar: [
+      {
+        title: 'JS基础',
+        path: '/js基础/',
+      },
+      {
+        title: 'CSS基础'
+      },
+      {
+        title: '网络通信'
+      },
+      {
+        title: '安全'
+      },
+      {
+        title: '前端框架'
+      },
+      {
+        title: '数据结构与算法'
+      },
+      {
+        title: '性能优化'
+      },
+      {
+        title: '前端工程化'
+      },
+      {
+        title: 'TypeScript'
+      },
+      {
+        title: 'Node'
+      },
+      {
+        title: '设计模式'
+      },
+      {
+        title: '项目/业务'
+      }
 
-    }
+    ]
   }
 }
