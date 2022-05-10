181e98be87ddefeaf833a1c9039cf59f0370e49d
王亮 <1392830517@qq.com>
2022-05-10 10:13:54 +0800

    feat: init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  title: 'Hello VuePress2', // 网站左上角的标题
+  description: 'Just playing around', // <meta name="description" content="Just playing around">
+  base: '/vuepress-frontend-web/',
+  configureWebpack: {
+    resolve: {
+
+    }
+  }
+}
